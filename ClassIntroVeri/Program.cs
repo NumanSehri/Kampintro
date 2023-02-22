9a05a6015ea746f2b0a3f934cbf465339699ad1e
NumanSehri <numansehri@gmail.com>
2023-02-22 16:36:33 +0300

    Kendi ürünlerimi ekleyip Forech ve While ile döndüm

---
+++ ClassIntroVeri/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassIntroVeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Numan";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Numan ŞEHRİ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "F#";
+            kurs2.Egitmen = "Burak Arif";
+            kurs2.IzlenmeOrani = 15;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "R#";
+            kurs3.Egitmen = "Arif İNCE";
+            kurs3.IzlenmeOrani = 18;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Muro";
+            kurs4.IzlenmeOrani = 100;
+
+
+            //Console.WriteLine(kurs1.KursAdi + ":" + kurs1.Egitmen);
+            //dizinin içinde kurs objesi tutuluyor. yani kurs class içindeki veriler tutuluyor
+            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3,kurs4};
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen + " Hoca");
+
+                         }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+
+    }
+}
---
+++ ProjectStudy/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ProjectStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.ProductName = "Monster Laptop";
+            product1.ProductPrice = 19000;
+            product1.ProductStock = 10;
+
+            Product product2 = new Product();
+            product2.ProductName = "Lg Monitör";
+            product2.ProductPrice = 4000;
+            product2.ProductStock = 5;
+
+            Product product3 = new Product();
+            product3.ProductName = "SSD 480gb";
+            product3.ProductPrice = 2300;
+            product3.ProductStock = 6;
+
+            Product product4 = new Product();
+            product4.ProductName = "Logitech klavye";
+            product4.ProductPrice = 1000;
+            product4.ProductStock = 4;
+            //4 adet ürünün adı, fiyatı ve stok durumu tutan class oluşturup 4 tane ürün ekledik
+
+            Product[] products = new Product[]
+            {product1,product2,product3,product4};
+            //Product larımı dizi tanımladık
+
+            foreach (var product in products)
+            {
+                Console.WriteLine("Ürün Adı:" + product.ProductName);
+                Console.WriteLine("Fiyatı:" + product.ProductPrice);
+                Console.WriteLine("Stok Durumu:" + product.ProductStock);
+            }
+            int i = 0;
+            
+            while (i<products.Length)
+            {
+                
+                Console.WriteLine("While Döngüsü ile yazıldı " + products[i].ProductName);
+                i++;
+            }
+
+            Console.WriteLine("Ürünlerimiz Listelendi");
+        }
+
+
+
+        public class Product
+        {
+            public string ProductName { get; set; }
+            public int ProductPrice { get; set; }
+            public int ProductStock { get; set; }
+
+
+
+        }
+            
+            
+
+
+    }
+
+
+}
