7e4acb2ced11eb3ab0370b4ddf7682f6cdcc7666
NumanSehri <numansehri@gmail.com>
2023-02-23 18:30:50 +0300

    Add,List,Delete Function Completed

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,31 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+
+            customer1.Id = 1;
+            customer1.CustomerName = "Numan";
+            customer1.CustomerSurname = "ŞEHRİ";
+            customer1.NationalIdentty = "12346789111";
+            customer1.Nation = "Türkiye";
+
+            Customer customer2 = new Customer();
+
+            customer2.Id = 2;
+            customer2.NationalIdentty = "23456789562";
+            customer2.CustomerName = "Damla";
+            customer2.CustomerSurname = "ŞEHRİ";
+            customer2.Nation = "Türkiye";
+
+            CustomerManager customerManager = new CustomerManager();
+            
+                customerManager.Add(customer1);
+                customerManager.List(customer1, customer2);
+                customerManager.Delete(customer1);
+
+
+
+            
         }
     }
 }
