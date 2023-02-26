63198715a1e3b615a387f3c1a5e718a65c6ce7ef
NumanSehri <numansehri@gmail.com>
2023-02-26 15:29:39 +0300

    Inheritance:Miras alma Class Sistemi Try

---
+++ Inhertance/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inhertance
+{
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+}
---
+++ Inhertance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Inhertance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[3]
+            {
+                new Customer{FirstName="Numan"},
+                new Student{FirstName="Damla"},
+                new Person{FirstName="Zeynep"}
+
+
+            };
+            foreach (var person in people)
+            {
+                Console.WriteLine("Kayıtlı Kişiler : " + person.FirstName);
+
+            }
+        }
+    }
+}
